0f78f0ceb29413ea5fbc5b15347d71d350008be1
RadioMehn <tiucoken77@gmail.com>
2026-04-24 04:07:02 +0800

    change to drinktropiko.shop

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://radiomehn.github.io/drinktropiko2',
+  siteUrl: 'https://drinktropiko.shop',
   generateRobotsTxt: false,
 };
