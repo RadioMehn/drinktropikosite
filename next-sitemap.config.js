1e1e25c69dadf827090b37e56b8dfd58925a1c90
RadioMehn <tiucoken77@gmail.com>
2026-04-24 03:59:15 +0800

    changed github name

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://radiomehn.github.io',
+  siteUrl: 'https://radiomehn.github.io/drinktropiko2',
   generateRobotsTxt: false,
 };
